84cc4109b8728d61e0655a195a3507f4ccb2b9a7
Gaurishhhh <146448869+Gaurishhhh@users.noreply.github.com>
2023-11-29 04:41:13 +0530

    Initial commit

---
+++ Spotify Clone/script.js
@@ -0,0 +1,15 @@
+console.log("Welcome to Spotify")
+let songs = [
+    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+
+]
+
+
+let audioElement = new Audio('1.mp3');
