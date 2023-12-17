18b985a509f2e4d1731699f9f498a391f0ad6fff
Gaurishhhh <146448869+Gaurishhhh@users.noreply.github.com>
2023-12-18 01:45:27 +0530

    Wrote all the Javascript

--- Spotify Clone/script.js
+++ Spotify Clone/script.js
@@ -1,15 +1,107 @@
-console.log("Welcome to Spotify")
-let songs = [
-    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Solitude", filepath:"song/1.mp3",coverPath:"covers/1.jpg"},
+console.log("Welcome to Spotify");
 
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Solitude", filepath:"songs/1.mp3",coverPath:"covers/cover1.jpg"},
+    {songName: "Terrified", filepath:"songs/2.mp3",coverPath:"covers/cover2.jpg"},
+    {songName: "Crime-slowed", filepath:"songs/3.mp3",coverPath:"covers/cover3.jpg"},
+    {songName: "The Moon Song", filepath:"songs/4.mp3",coverPath:"covers/cover4.jpg"},
+    {songName: "Lavender Azooz", filepath:"songs/5.mp3",coverPath:"covers/cover5.jpg"},
+    {songName: "Out like a light 2", filepath:"songs/6.mp3",coverPath:"covers/cover6.jpg"},
+    {songName: "Mariela!?", filepath:"songs/7.mp3",coverPath:"covers/cover7.jpg"},
+    {songName: "Perfect", filepath:"songs/8.mp3",coverPath:"covers/cover8.jpg"},
 ]
 
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
 
-let audioElement = new Audio('1.mp3');
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
